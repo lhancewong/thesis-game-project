f406e4e8b9376bdbb479f8884573e35581314e2d
Lhance Wong <lhance.wong@obf.ateneo.edu>
2023-08-30 09:53:03 +0800

    Update BGM to play globally instead of only in title screen

--- interfaces/title_screen_menus/scripts/title_screen_main.gd
+++ interfaces/title_screen_menus/scripts/title_screen_main.gd
@@ -5,16 +5,6 @@ onready var vine_boom = $VineBoom
 onready var BGM = $BGM
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$BGM.play()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Start_pressed():
 	get_tree().change_scene("res://interfaces/title_screen_menus/title_screen_save_selection.tscn")
 
