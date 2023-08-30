9e1085c4c27704d93f0a25a1c7ea411ec14db336
Lhance Wong <lhance.wong@obf.ateneo.edu>
2023-08-30 09:55:02 +0800

    Init UX and routing for on_daylight_main

--- interfaces/on_daylight_menus/scripts/on_daylight_main.gd
+++ interfaces/on_daylight_menus/scripts/on_daylight_main.gd
@@ -1,9 +1,13 @@
 extends Control
 
 
+onready var villager_sigh = $VillagerSigh
+onready var BGM = $BGM
+
+
 func _on_ToTitleScreen_pressed():
 	get_tree().change_scene("res://interfaces/title_screen_menus/title_screen_main.tscn")
 
 
-func _on_SaveFile1_pressed():
-	get_tree().change_scene("res://interfaces/on_daylight_menus/on_daylight_main.tscn")
+func _on_StartDay_pressed():
+	villager_sigh.play()
