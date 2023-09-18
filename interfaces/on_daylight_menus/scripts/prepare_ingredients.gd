1daf7bcc5d2406e659c719fbf7840d3d8e4dc039
Wilbert Chen <wilbertmchen@gmail.com>
2023-09-18 18:08:46 +0800

    added a bit of button functionality for prepare_ingredients

--- interfaces/on_daylight_menus/scripts/prepare_ingredients.gd
+++ interfaces/on_daylight_menus/scripts/prepare_ingredients.gd
@@ -1,13 +1,15 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Resources
-var food1 = 0
+export var stock_ingredient1 = 0
+export var stock_ingredient2 = 0
+
+export var food1 = 0
+export var food2 = 0
+
+export var prepared_ingredient1 = 0
+export var prepared_ingredient2 = 0
 
 # SFX
 onready var villager_sigh = $VillagerSigh
@@ -19,13 +21,29 @@ func _ready():
 
 
 func _on_food1plus_pressed():
+	food1 = int($MainContainer/MiddleLine/Middle1/HBoxContainer/LineEdit.text)
 	food1 += 1
 	$MainContainer/MiddleLine/Middle1/HBoxContainer/LineEdit.text = str(food1)
 	
 
 
 func _on_food1minus_pressed():
+	food1 = int($MainContainer/MiddleLine/Middle1/HBoxContainer/LineEdit.text)
 	if food1 == 0:
 		return
 	food1 -= 1
 	$MainContainer/MiddleLine/Middle1/HBoxContainer/LineEdit.text = str(food1)
+
+
+func _on_food2plus_pressed():
+	food2 = int($MainContainer/MiddleLine/Middle2/HBoxContainer/LineEdit.text)
+	food2 += 1
+	$MainContainer/MiddleLine/Middle2/HBoxContainer/LineEdit.text = str(food2)
+
+func _on_food2minus_pressed():
+	food2 = int($MainContainer/MiddleLine/Middle2/HBoxContainer/LineEdit.text)
+	if food2 == 0:
+		return
+	food2 -= 1
+	$MainContainer/MiddleLine/Middle2/HBoxContainer/LineEdit.text = str(food2)
+
