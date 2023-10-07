0ed7971ad5188913f95b91d71572f8461216076e
Jeremy Lee <jeremy.lee@obf.ateneo.edu>
2023-10-07 21:13:11 +0800

    feat: add theme and update UI

--- interfaces/on_daylight_menus/scripts/subscenes/compost.gd
+++ interfaces/on_daylight_menus/scripts/subscenes/compost.gd
@@ -12,15 +12,12 @@ var compost_amount = 0
 func _ready():
 	$VBoxContainer/HBoxContainer/Compost/Waste/Amount/InedibleWasteAmnt.text = str(waste_amount)
 	$VBoxContainer/HBoxContainer/Compost/Strategy/Amount/CompostAmnt.text = str(compost_amount)
-	$VBoxContainer/HBoxContainer/Compost/Strategy/HSlider.max_value = waste_amount
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	waste_amount = waste_handler.get_inedible_waste()
 	$VBoxContainer/HBoxContainer/Compost/Waste/Amount/InedibleWasteAmnt.text = str(waste_amount)
 	$VBoxContainer/HBoxContainer/Compost/Strategy/Amount/CompostAmnt.text = str(compost_amount)
-	$VBoxContainer/HBoxContainer/Compost/Strategy/HSlider.max_value = float(waste_amount)
 
 # This changes the value of the amount to be composted using the slider
 func _on_HSlider_value_changed(value):
@@ -32,4 +29,3 @@ func _on_sendButton_pressed():
 		return
 	waste_handler.manage_waste("composting", 1, compost_amount, 1)
 	compost_amount = 0
-	$VBoxContainer/HBoxContainer/Compost/Strategy/HSlider.value = 0
--- interfaces/on_daylight_menus/scripts/subscenes/landfill.gd
+++ interfaces/on_daylight_menus/scripts/subscenes/landfill.gd
@@ -7,20 +7,15 @@ var inedibleToLandfill = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/HBoxContainer/VBoxContainer/InedibleWaste/HSlider.max_value = float(waste_handler.get_inedible_waste())
-	$"VBoxContainer/HBoxContainer/VBoxContainer/Edible Waste/HSlider".max_value = float(waste_handler.get_edible_waste())
-	$VBoxContainer/HBoxContainer/VBoxContainer/InedibleWaste/Amount/InedibleAmnt.text = str(inedibleToLandfill)
-	$"VBoxContainer/HBoxContainer/VBoxContainer/Edible Waste/Amount/EdibleAmnt".text = str(edibleToLandfill)
-
-
-
+	_update_labels()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	$VBoxContainer/HBoxContainer/VBoxContainer/InedibleWaste/HSlider.max_value = float(waste_handler.get_inedible_waste())
-	$"VBoxContainer/HBoxContainer/VBoxContainer/Edible Waste/HSlider".max_value = float(waste_handler.get_edible_waste())
-	$VBoxContainer/HBoxContainer/VBoxContainer/InedibleWaste/Amount/InedibleAmnt.text = str(inedibleToLandfill)
-	$"VBoxContainer/HBoxContainer/VBoxContainer/Edible Waste/Amount/EdibleAmnt".text = str(edibleToLandfill)
+	_update_labels()
+
+func _update_labels():
+	$VBoxContainer/HBoxContainer/Landfill/InedibleWaste/Amount/InedibleAmnt.text = str(inedibleToLandfill)
+	$VBoxContainer/HBoxContainer/Landfill/EdibleWaste/Amount/EdibleAmnt.text = str(edibleToLandfill)
 
 
 func _on_InedibleSlider_value_changed(value):
@@ -45,7 +40,5 @@ func _on_TrashButton_pressed():
 			print(waste_handler.get_inedible_waste())
 		edibleToLandfill = 0
 		inedibleToLandfill = 0
-		$"VBoxContainer/HBoxContainer/VBoxContainer/Edible Waste/HSlider".value = 0
-		$VBoxContainer/HBoxContainer/VBoxContainer/InedibleWaste/HSlider.value = 0
 	
 	
