54ca5eb2258bc3474125ea7050070841a8857e36
Wilbert Chen <wilbertmchen@gmail.com>
2023-11-07 01:05:10 +0800

    fixed compost and order_ingredients pages

--- interfaces/on_daylight_menus/scripts/subscenes/compost.gd
+++ interfaces/on_daylight_menus/scripts/subscenes/compost.gd
@@ -1,26 +1,53 @@
 extends Control
+var regex = RegEx.new()
 
 var waste_amount = 0
 var compost_amount = 0
+var old_text = ""
+
+onready var CompostLineEdit = $VBoxContainer/HBoxContainer/Compost/Strategy/HBoxContainer2/LineEdit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $VBoxContainer/HBoxContainer/Compost/Waste/Amount/InedibleWasteAmnt.text = str(waste_amount)
-    $VBoxContainer/HBoxContainer/Compost/Strategy/Amount/CompostAmnt.text = str(compost_amount)
+	$VBoxContainer/HBoxContainer/Compost/Waste/Amount/InedibleWasteAmnt.text = str(waste_amount)
+	CompostLineEdit.text = str(compost_amount)
+	regex.compile("^[0-9]*$")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-    waste_amount = Game.waste_hndlr.get_inedible_waste()
-    $VBoxContainer/HBoxContainer/Compost/Waste/Amount/InedibleWasteAmnt.text = str(waste_amount)
-    $VBoxContainer/HBoxContainer/Compost/Strategy/Amount/CompostAmnt.text = str(compost_amount)
+	waste_amount = Game.waste_hndlr.get_inedible_waste()
+	$VBoxContainer/HBoxContainer/Compost/Waste/Amount/InedibleWasteAmnt.text = str(waste_amount)
 
-# This changes the value of the amount to be composted using the slider
-func _on_HSlider_value_changed(value):
-    compost_amount = $VBoxContainer/HBoxContainer/Compost/Strategy/HSlider.value
+# This changes the value of the amount to be composted using the buttons
 
-# Calls manage_waste and resets slider value
+
+# Calls manage_waste and resets value
 func _on_sendButton_pressed():
-    if compost_amount == 0:
-        return
-    Game.waste_hndlr.manage_waste("composting", 1, compost_amount, 1)
-    compost_amount = 0
+	if compost_amount == 0:
+		return
+	Game.waste_hndlr.manage_waste("composting", 0, compost_amount, 1)
+	compost_amount = 0
+	CompostLineEdit.text = str(compost_amount)
+
+
+func _on_LineEdit_text_changed(new_text):
+	if regex.search(new_text):
+		old_text = str(new_text)
+#		beef_amount = int(new_text)
+	else:
+		CompostLineEdit.text = old_text
+		CompostLineEdit.set_cursor_position(CompostLineEdit.text.length())
+
+
+func _on_minus_pressed():
+	if compost_amount == 0:
+		return
+	else:
+		compost_amount -= 1
+		CompostLineEdit.text = str(compost_amount)
+	
+
+
+func _on_plus_pressed():
+	compost_amount += 1
+	CompostLineEdit.text = str(compost_amount)
